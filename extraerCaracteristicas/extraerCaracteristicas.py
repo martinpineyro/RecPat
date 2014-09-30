0d5a0badfac0e14f36585ef6601602a2875ad386
juanibraun <juanibraun@gmail.com>
2014-09-30 00:43:41 -0300

    se modifica extraerCaracteristicas.py, ahora se le pasa como entrada el nombre de la carpeta donde estan las fotos y genera en esta misma carpeta el archivo con las caracteristicas encontradas para todas las fotos. El nombre de archivo con las caracteristicas es el mismo que el de la imagen.

--- extraerCaracteristicas/extraerCaracteristicas.py
+++ extraerCaracteristicas/extraerCaracteristicas.py
@@ -1,52 +1,58 @@
-import numpy as np
-import cv2
-import sys
-import csv
+def extraerCaracteristica(name, factor):
 
+    import numpy as np
+    import cv2
+    import csv
+    import string
+    #abro imagen y la muestro
+    print name    
+    img = cv2.imread(name,0)
+    #cv2.imshow('Demo',img)
+    height,width = img.shape
+    
+    # creo archivo para guardar descriptores
+    out_name = string.replace(name,'jpg','csv')
+   
+    #print out_name
+    with open(out_name,'wb') as f:
+        writer = csv.writer(f,delimiter=';')
+        #arranco a recorrer por bloques
+        for i in range(0, factor):
+            for j in range(0, factor):
+                #elijo bloque de la imagen
+                aux=img[i*height/factor:i*height/factor+height/factor-1,j*width/factor:j*width/factor+width/factor-1]
+ 
+            	"""
+            	#muestro bloque
+            	cv2.imshow('Demo2',aux)
+            	k=cv2.waitKey(0)
+            	if k == ord('c'):
+            	cv2.destroyWindow('Demo2')
+            	"""
+            		
+            		    	# SURF 
+            	surf = cv2.SURF(500)
+            	surf.extended = False
+            	kp, descritors = surf.detectAndCompute(aux,None)
+            	if descritors != None:
+                    	writer.writerow(['bloque',i*factor+j,'numero descriptores', len(descritors)])                
+                    	writer.writerows(descritors)
+   
+import os
+import sys
 
 #leo parametros de entrada
-filename = sys.argv[-2]
+path = sys.argv[-2]
 factor = sys.argv[-1]
 factor = int(factor)
 
-#abro imagen y la muestro
-img = cv2.imread(filename,0)
-#cv2.imshow('Demo',img)
-height,width = img.shape
-
-# creo archivo para guardar descriptores
-out = 
-with open('out.csv', 'wb') as f:
-    writer = csv.writer(f,delimiter=';')
-    #arranco a recorrer por bloques
-    for i in range(0, factor):
-        for j in range(0, factor):
-            print i,j
-            #elijo bloque de la imagen
-            aux=img[i*height/factor:i*height/factor+height/factor-1,j*width/factor:j*width/factor+width/factor-1]
-     
-            """
-            #muestro bloque
-            cv2.imshow('Demo2',aux)
-            k=cv2.waitKey(0)
-            if k == ord('c'):
-                cv2.destroyWindow('Demo2')
-            """
-		
-		    # SURF 
-            surf = cv2.SURF(500)
-            surf.extended = False
-            kp, descritors = surf.detectAndCompute(aux,None)
-            if descritors != None:
-                print len(descritors)                
-                writer.writerows(descritors)
-           
+for file in os.listdir(path):
+    current_file = os.path.join(path, file)
+    print current_file
+    extraerCaracteristica(current_file, factor)
     
-        
 
-def cerrarVentana():
-    cv2.destroyAllWindows()
-    sys.exit()
+
 
 
 
